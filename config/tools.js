1d40d31262745a1a10ebfb577b35852c3123180a
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-02-12 22:04:45 -0700

    fix(Config): Fix config

--- config/tools.js
+++ config/tools.js
@@ -1,15 +1,12 @@
 module.exports = {
-    name: 'example',
-    dep: {
-        build: {
-            "node-libs-browser": "1.0.0",
-            "webpack": "1.12.12",
-        },
-        test: {
-            "expect":"1.13.4",
-        },
-        "utils": {
-            "shelljs": "0.5.3",
-        },
+    build: {
+        "node-libs-browser": "1.0.0",
+        "webpack": "1.12.12",
     },
+    test: {
+        "expect":"1.13.4",
+    },
+    "utils": {
+        "shelljs": "0.5.3",
+    }
 }
